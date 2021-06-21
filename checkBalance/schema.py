95840785c84cb60081e06ac458ffb5567e661dca
Резанова Александра <rezanovasasha@gmail.com>
2021-06-21 20:05:25 +0300

    removed registery and all email connections

--- checkBalance/schema.py
+++ checkBalance/schema.py
@@ -10,14 +10,6 @@ import budget.mutations
 
 class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
-    verify_account = mutations.VerifyAccount.Field()
-    resend_activation_email = mutations.ResendActivationEmail.Field()
-    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
-    password_reset = mutations.PasswordReset.Field()
-    password_change = mutations.PasswordChange.Field()
-    archive_account = mutations.ArchiveAccount.Field()
-    delete_account = mutations.DeleteAccount.Field()
-    update_account = mutations.UpdateAccount.Field()
 
     # django-graphql-jwt inheritances
     token_auth = mutations.ObtainJSONWebToken.Field()
