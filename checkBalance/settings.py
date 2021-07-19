0ab47943ce25d180ea9d1dc7a35fef1931e570f8
srezanova <rezanovasasha@gmail.com>
2021-07-19 13:21:27 +0300

    fixed debug

--- checkBalance/settings.py
+++ checkBalance/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = env('DEBUG')
+DEBUG = (env('DEBUG') == 'True')
 
 ALLOWED_HOSTS = [
     '143.198.170.199',
