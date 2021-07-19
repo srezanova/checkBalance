c466cccf606fde3e49872a715fd2677e19e16ec7
srezanova <rezanovasasha@gmail.com>
2021-07-19 17:51:27 +0300

    refresh token

--- checkBalance/settings.py
+++ checkBalance/settings.py
@@ -114,7 +114,7 @@ AUTHENTICATION_BACKENDS = [
 GRAPHQL_JWT = {
     "JWT_VERIFY_EXPIRATION": True,
     "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
-    'JWT_EXPIRATION_DELTA': timedelta(days=30),
+    'JWT_EXPIRATION_DELTA': timedelta(minutes=60),
     'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=50),
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
     'JWT_SECRET_KEY': env('JWT_KEY'),
